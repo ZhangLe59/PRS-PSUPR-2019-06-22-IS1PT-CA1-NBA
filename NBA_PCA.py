df534879aef89829abcbf112fce2dd77c5180484
ZhangLe59 <30823899+ZhangLe59@users.noreply.github.com>
2019-08-24 20:05:25 +0800

    create pca_nba.py
    create pca_nba.py

---
+++ NBA_PCA.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
